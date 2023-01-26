a664391659c5200385ab7472acf8336579e2647a
irvanshandika <shandikamuhammadirvan@gmail>
2023-01-26 22:52:36 +0700

    Fix

--- src/components/carousel.js
+++ src/components/carousel.js
@@ -7,16 +7,16 @@ function Carousel() {
         <div id="carouselExampleSlidesOnly" className="carousel slide" data-bs-ride="carousel">
           <div className="carousel-inner">
             <div className="carousel-item active">
-              <img src="https://projektoko.vercel.app/assets/images/facemask.jpg" className="carousel-images d-block mx-auto" alt="..." />
+              <img src="https://projektoko.vercel.app/assets/images/facemask.jpg" className="fotocar d-block mx-auto" alt="..." />
             </div>
             <div className="carousel-item">
-              <img src="https://projektoko.vercel.app/assets/images/leau.jpg" className="carousel-images d-block mx-auto" alt="..." />
+              <img src="https://projektoko.vercel.app/assets/images/leau.jpg" className="fotocar dblockx mx-auto" alt="..." />
             </div>
             <div className="carousel-item">
-              <img src="https://projektoko.vercel.app/assets/images/spray.jpg" className="carousel-images d-block mx-auto" alt="..." />
+              <img src="https://projektoko.vercel.app/assets/images/spray.jpg" className="fotocar d-block mx-auto" alt="..." />
             </div>
             <div className="carousel-item">
-              <img src="https://projektoko.vercel.app/assets/images/bodyspa.jpg" className="carousel-images d-block mx-auto" alt="..." />
+              <img src="https://projektoko.vercel.app/assets/images/bodyspa.jpg" className="fotocar dblockx mx-auto" alt="..." />
             </div>
           </div>
         </div>
--- src/components/layanan.js
+++ src/components/layanan.js
@@ -6,18 +6,20 @@ import Packaging from "../packaging.png";
 function Layanan() {
   return (
     <>
-      <div className="layanan container px-4 text-center">
-        <div className="kurir">
-          <img src={Shipping} alt="Shipping" className="images-layanan" />
-          <p className="deskripsi text-center">Menggunakan Ekspedisi Yang Aman Dan Terpercaya</p>
-        </div>
-        <div className="admin">
-          <img src={Admin} className="images-layanan" alt="Admin" />
-          <p className="deskripsi text-center">Memberikan Pelayanan Chat Admin Yang Cepat Tanggap</p>
-        </div>
-        <div className="packaging">
-          <img src={Packaging} className="images-layanan" alt="Admin" />
-          <p className="deskripsi text-center">Memberikan Ekstra Pengemasan Dalam Pengiriman</p>
+      <div className="layanan">
+        <div className="container px-4 text-center">
+          <div className="kurir">
+            <img src={Shipping} alt="Shipping" className="images-layanan" />
+            <p className="deskripsi text-center">Menggunakan Ekspedisi Yang Aman Dan Terpercaya</p>
+          </div>
+          <div className="admin">
+            <img src={Admin} className="images-layanan" alt="Admin" />
+            <p className="deskripsi text-center">Memberikan Pelayanan Chat Admin Yang Cepat Tanggap</p>
+          </div>
+          <div className="packaging">
+            <img src={Packaging} className="images-layanan" alt="Admin" />
+            <p className="deskripsi text-center">Memberikan Ekstra Pengemasan Dalam Pengiriman</p>
+          </div>
         </div>
       </div>
     </>
